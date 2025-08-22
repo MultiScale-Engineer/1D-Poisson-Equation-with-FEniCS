47bd390b47a75adfef95d033729d3f9e564dc866
MultiScale Engineer <sina.taghizadeh12@gmail.com>
2025-08-22 16:12:20 +0330

    Create PoissonEquation.py

---
+++ PoissonEquation.py
@@ -0,0 +1,29 @@
+from dolfin import *
+
+#Define mesh
+N=10
+mesh = UnitIntervalMesh(N)
+
+#Define space of elements
+V = FunctionSpace(mesh, 'P', 1)
+u = TrialFunction(V)
+v = TestFunction(V)
+
+#Define BCs
+u_D = Expression('0', degree=1)
+def boundary(x, on_boundary):
+    return on_boundary
+bc = DirichletBC(V, u_D, boundary)
+
+#Define variational problem
+f = Constant(-1.0)
+a = dot(grad(u), grad(v))*dx
+L = f*v*dx
+
+#Solve PDE
+u = Function(V)
+solve(a == L, u, bc)
+
+#Visualize the output
+file = File("poisson_solution.pvd")
+file << u
